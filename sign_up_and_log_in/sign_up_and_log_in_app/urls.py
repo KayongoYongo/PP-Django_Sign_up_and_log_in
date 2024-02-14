82981554e89ee6862df91d96b6b361932a3f1e80
KayongoYongo <sknsammie10@gmail.com>
2024-02-14 22:34:11 +0300

    updated the views and urls

--- sign_up_and_log_in/sign_up_and_log_in_app/urls.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, sign_up_page
 
 urlpatterns = [
         path('', home, name='home'),
+        path('sign_up_page', sign_up_page, name='sign_up_page')
 ]
--- sign_up_and_log_in/sign_up_and_log_in_app/views.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import User
+from .forms import UserForm
 
 # Create your views here.
 def home(request):
     return render(request, 'sign_up_and_log_in_app/index.html')
+
+def sign_up_page(request):
+    form = UserForm()
+    return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
+
+def sign_up(request):
+    pass
