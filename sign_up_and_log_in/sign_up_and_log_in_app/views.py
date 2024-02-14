847cfed029b12718151db273868efa8e1e81b954
KayongoYongo <sknsammie10@gmail.com>
2024-02-14 02:40:44 +0000

    Added update to home

--- sign_up_and_log_in/sign_up_and_log_in_app/views.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(reqest, 'sign_up_and_log_in_app/index.html')
+    return render(request, 'sign_up_and_log_in_app/index.html')
