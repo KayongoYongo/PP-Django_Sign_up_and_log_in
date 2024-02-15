8860543037b2559e9e31740e3bf206bcc802e94b
KayongoYongo <sknsammie10@gmail.com>
2024-02-15 04:01:14 +0300

    updated views

--- sign_up_and_log_in/sign_up_and_log_in_app/views.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 from .signUpForm import UserForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -10,5 +12,23 @@ def sign_up_page(request):
     form = UserForm()
     return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
 
+def log_in_page(request):
+    return render(request, 'sign_up_and_log_in_app/log_in.html')
+
 def sign_up(request):
-    pass
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            second_name = form.cleaned_data['second_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            User.objects.create_user(first_name=first_name, second_name=second_name, email=email, phone_number=phone_number, password=password)
+            # user = authenticate(email=email, password=password)
+            messages.success(request, "Sign up successful! Please log in")
+            # login(request, user)
+            return redirect('log_in_page')
+    else:
+        form = UserForm(request.POST)
+        return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
