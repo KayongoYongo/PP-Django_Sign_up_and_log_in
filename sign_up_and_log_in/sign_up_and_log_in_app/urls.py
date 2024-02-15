c263a619019f07ca607e53828d37d06fdb251732
KayongoYongo <sknsammie10@gmail.com>
2024-02-15 04:01:03 +0300

    added urls

--- sign_up_and_log_in/sign_up_and_log_in_app/urls.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, sign_up_page
+from .views import home, sign_up_page, log_in_page
 
 urlpatterns = [
         path('', home, name='home'),
-        path('sign_up_page', sign_up_page, name='sign_up_page')
+        path('sign_up_page', sign_up_page, name='sign_up_page'),
+        path('log_in_page', log_in_page, name='log_in_page')
 ]
