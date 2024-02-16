6feec61cb36238ad14978d2ca4a0618845b7f70a
KayongoYongo <sknsammie10@gmail.com>
2024-02-16 06:44:43 +0300

    updated the views and urls

--- sign_up_and_log_in/sign_up_and_log_in_app/views.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/views.py
@@ -16,17 +16,19 @@ def log_in_page(request):
     return render(request, 'sign_up_and_log_in_app/log_in.html')
 
 def sign_up(request):
+    print(UserForm)
     if request.method == 'POST':
         form = UserForm(request.POST)
+        print(form.data)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            second_name = form.cleaned_data['second_name']
+            user_name = form.cleaned_data['user_name']
             email = form.cleaned_data['email']
-            phone_number = form.cleaned_data['phone_number']
-            hashed_password = form.cleaned_data['hashed_password']
-            User.objects.create(first_name=first_name, second_name=second_name, email=email, phone_number=phone_number, hashed_password=hashed_password)
+            password = form.cleaned_data['password']
+            User.objects.create(user_name=user_name, email=email, password=password)
             messages.success(request, "Sign up successful! Please log in")
+            print(user_name,email,password)
             return redirect('log_in_page')
     else:
-        form = UserForm(request.POST)
-        return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
+        form = UserForm()
+        
+    return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
