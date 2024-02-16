76e24cb0fec44f9479a894d4216b207c5220c632
KayongoYongo <sknsammie10@gmail.com>
2024-02-16 06:43:56 +0300

    added sign up form

--- sign_up_and_log_in/sign_up_and_log_in_app/signUpForm.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/signUpForm.py
@@ -1,29 +1,58 @@
 from django.forms import ModelForm, TextInput
+from django.core.validators import MinLengthValidator
+from django import forms
 from .models import User
 
 class UserForm(ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), required=False)
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}), required=False)
+
     class Meta:
         model = User
         fields = [
-            "first_name",
-            "second_name",
+            "user_name",
             "email", 
-            "phone_number", 
-            "hashed_password"
         ]
-        
+
         widgets = {
-            'first_name': TextInput(attrs={'placeholder': 'First Name'}),
-            'second_name': TextInput(attrs={'placeholder': 'Second Name'}),
+            'user_name': TextInput(attrs={'placeholder': 'User Name'}),
             'email': TextInput(attrs={'placeholder': 'Enter Email'}),
-            'phone_number': TextInput(attrs={'placeholder': 'Phone Number'}),
-            'hashed_password': TextInput(attrs={'placeholder': 'Password'}),
+            'password': TextInput(attrs={'placeholder': 'Password'}),
         }
-        
+
         labels = {
-            "first_name": '',
-            "second_name": '',
+            "user_name": '',
             "email": '', 
-            "phone_number": '', 
-            "hashed_password": ''
         }
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
+        # Adding a validator to enforce minimum password length
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email is None or email.strip() == '':
+            raise forms.ValidationError("The email cannot remain empty")
+        return email
+    
+    def clean_user_name(self):
+        user_name =  self.cleaned_data.get('user_name')
+
+        if user_name is None or user_name.strip() == '':
+            raise forms.ValidationError("The user name cannot remian empty")
+        return user_name
+    
+    
+    def clean_password(self):
+        password1 = self.cleaned_data.get("password1")
+
+        if password1 is None or password1.strip() == '':
+            raise forms.ValidationError("The password cannot remian empty")
+        
+        if len(password1) < 6:
+            raise forms.ValidationError("The password cannot be less than 6 characters")
+        
+        return password1
