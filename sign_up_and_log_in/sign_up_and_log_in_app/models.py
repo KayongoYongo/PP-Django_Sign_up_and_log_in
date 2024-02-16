906d75287d368a5d1edc4e08d2f0327d9bca5c40
KayongoYongo <sknsammie10@gmail.com>
2024-02-16 06:43:33 +0300

    added models

--- sign_up_and_log_in/sign_up_and_log_in_app/models.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/models.py
@@ -3,11 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(unique=True)
-    hashed_password = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=50)
-    second_name = models.CharField(max_length=50, blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True)
+    password = models.CharField(max_length=128, blank=True)
+    user_name = models.CharField(max_length=50, blank=True)
     session_id = models.CharField(max_length=100, blank=True, null=True)
     reset_token = models.CharField(max_length=100, blank=True, null=True)
     date_joined = models.DateTimeField(default=timezone.now)
