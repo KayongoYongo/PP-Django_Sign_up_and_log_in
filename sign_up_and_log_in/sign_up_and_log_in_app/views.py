28554e85278da2d985b1f341708a24bbf39d6a2e
KayongoYongo <sknsammie10@gmail.com>
2024-02-18 22:10:44 +0300

    added views functionality

--- sign_up_and_log_in/sign_up_and_log_in_app/views.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import User
-from .signUpForm import UserForm
+from .signUpForm import signUpForm
+from .logInForm import logInForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password
@@ -11,16 +12,17 @@ def home(request):
     return render(request, 'sign_up_and_log_in_app/index.html')
 
 def sign_up_page(request):
-    form = UserForm()
+    form = signUpForm()
     return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
 
 def log_in_page(request):
-    return render(request, 'sign_up_and_log_in_app/log_in.html')
+    form = logInForm()
+    return render(request, 'sign_up_and_log_in_app/log_in.html', {'form': form})
 
 def sign_up(request):
-    print(UserForm)
+    print(signUpForm)
     if request.method == 'POST':
-        form = UserForm(request.POST)
+        form = signUpForm(request.POST)
         print(form.data)
         if form.is_valid():
             user_name = form.cleaned_data['user_name']
@@ -32,6 +34,28 @@ def sign_up(request):
             print(user_name,email,password)
             return redirect('log_in_page')
     else:
-        form = UserForm()
+        form = signUpForm()
         
     return render(request, 'sign_up_and_log_in_app/sign_up.html', {'form' : form})
+
+def log_in(request):
+    print(logInForm)
+    if request.method == 'POST':
+        form = logInForm(request.POST)
+        print(form.data)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Log in successful!")
+                return redirect('home')
+            else:
+                # Authentication failed
+                error_message = "Invalid username or password."
+                return render(request, 'sign_up_and_log_in_app/log_in.html', {'error_message': error_message})
+    else:
+        form = logInForm()
+    
+    return render(request, 'sign_up_and_log_in_app/log_in.html', {'form' : form})
