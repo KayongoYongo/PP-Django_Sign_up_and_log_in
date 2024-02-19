31266614d5c4fdd7ee04448f2a1e471fef73a09d
KayongoYongo <sknsammie10@gmail.com>
2024-02-19 07:32:45 +0300

    fixed the models

--- sign_up_and_log_in/sign_up_and_log_in_app/models.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(unique=True, blank=True, error_messages={'unique':"This email is already registered."})
+    email = models.EmailField(blank=True)
     password = models.CharField(max_length=128, blank=True)
     user_name = models.CharField(max_length=50, blank=True)
     session_id = models.CharField(max_length=100, blank=True, null=True)
