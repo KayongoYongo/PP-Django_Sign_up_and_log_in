276641d8527db7e1151dcd586a2bedb32b908a97
KayongoYongo <sknsammie10@gmail.com>
2024-02-21 08:15:40 +0300

    modified log in form

--- sign_up_and_log_in/sign_up_and_log_in_app/logInForm.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/logInForm.py
@@ -1,23 +1,17 @@
 from django import forms
-from django.forms import ModelForm
-from .models import User
 
-class logInForm(ModelForm):
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter Email', 'class': 'form-control'}), required=False)
+class logInForm(forms.Form):
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'}), required=False)
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}), required=False)
 
-    class Meta:
-        model = User
-        fields = ['email', 'password']
-
     def clean_email(self):
-        email = self.cleaned_data.get('email')
+        email= self.cleaned_data.get('email')
 
         if email is None or email.strip() == '':
-            raise forms.ValidationError("The email cannot remain empty")
+            raise forms.ValidationError("The username cannot remain empty")
         
         return email
-    
+
     def clean_password(self):
         password = self.cleaned_data.get("password")
 
