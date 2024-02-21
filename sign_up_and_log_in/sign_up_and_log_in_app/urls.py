ad03608320009777eb1d52e2728eb314d7524690
KayongoYongo <sknsammie10@gmail.com>
2024-02-21 08:15:13 +0300

    added new logout url

--- sign_up_and_log_in/sign_up_and_log_in_app/urls.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, sign_up_page, sign_up, log_in_page, log_in
+from .views import home, sign_up_page, sign_up, log_in_page, log_in, logout_view
 
 urlpatterns = [
         path('', home, name='home'),
@@ -7,4 +7,5 @@ urlpatterns = [
         path('sign_up', sign_up, name='sign_up'),
         path('log_in_page', log_in_page, name='log_in_page'),
         path('log_in', log_in, name='log_in'),
+        path('logout/', logout_view, name='logout'),
 ]
