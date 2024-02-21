153a810ea1efad3a30f20c6b1927adda29e6da14
KayongoYongo <sknsammie10@gmail.com>
2024-02-21 08:15:29 +0300

    updated views functionality

--- sign_up_and_log_in/sign_up_and_log_in_app/views.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import User
 from .signUpForm import signUpForm
 from .logInForm import logInForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.hashers import make_password
-from django.core.exceptions import ValidationError
 
 # Create your views here.
 def home(request):
@@ -25,14 +23,14 @@ def sign_up(request):
         form = signUpForm(request.POST)
         print(form.data)
         if form.is_valid():
-            user_name = form.cleaned_data['user_name']
+            username = form.cleaned_data['username']
             email = form.cleaned_data['email']
-            password = form.cleaned_data['password_1']
-            hashed_password = make_password(password)       
-            User.objects.create(user_name=user_name, email=email, password=hashed_password)
+            password = form.cleaned_data['password1']
+            User.objects.create_user(username=username, email=email, password=password)
             messages.success(request, "Sign up successful! Please log in")
-            print(user_name,email,password)
+            print(username,email,password)
             return redirect('log_in_page')
+        print(len(form.errors))
     else:
         form = signUpForm()
         
@@ -43,19 +41,42 @@ def log_in(request):
     if request.method == 'POST':
         form = logInForm(request.POST)
         print(form.data)
+
         if form.is_valid():
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            user = authenticate(request, email=email, password=password)
+
+            # Check if a user with the provided email exists
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                user = None
+
             if user is not None:
-                login(request, user)
-                messages.success(request, "Log in successful!")
-                return redirect('home')
+                user = authenticate(request, username=user.username, password=password)
+                print("The user is", user)
+
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, "Log in successful!")
+                    return redirect('home')
+                else:
+                    # Authentication failed
+                    print('wrong credentials')
+                    messages.error(request, "Invalid email or password")
+                    return redirect('log_in_page')
             else:
-                # Authentication failed
-                error_message = "Invalid username or password."
-                return render(request, 'sign_up_and_log_in_app/log_in.html', {'error_message': error_message})
+                # User with the provided email does not exist
+                messages.error(request, "No user with this email exists")
+
+        # Form data invalid, display form with errors
+        return render(request, 'sign_up_and_log_in_app/log_in.html', {'form': form})
     else:
+        # GET request, display empty form
         form = logInForm()
-    
     return render(request, 'sign_up_and_log_in_app/log_in.html', {'form' : form})
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a desired page after logout
+    return redirect('home')
