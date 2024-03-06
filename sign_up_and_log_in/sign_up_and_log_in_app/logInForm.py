db069bd494f583caaf729c8f810503dc59ee4d0f
KayongoYongo <sknsammie10@gmail.com>
2024-03-06 05:19:34 +0300

    added the remember me radio button

--- sign_up_and_log_in/sign_up_and_log_in_app/logInForm.py
+++ sign_up_and_log_in/sign_up_and_log_in_app/logInForm.py
@@ -3,6 +3,7 @@ from django import forms
 class logInForm(forms.Form):
     email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'form-control'}), required=False)
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}), required=False)
+    remember_me = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
 
     def clean_email(self):
         email= self.cleaned_data.get('email')
