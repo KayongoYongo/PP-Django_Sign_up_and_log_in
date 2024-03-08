c8cd9be209bb990b77ba38dc0264d29292038c28
KayongoYongo <sknsammie10@gmail.com>
2024-03-08 23:16:05 +0300

    added a comment for SMTP

--- sign_up_and_log_in/sign_up_and_log_in/settings.py
+++ sign_up_and_log_in/sign_up_and_log_in/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# Create SMTP for email
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z*&ag@dt*jy9jjvq=j9l2=z&6@f(1b*lk4bv3=iet3_a+bt)!1'
 
