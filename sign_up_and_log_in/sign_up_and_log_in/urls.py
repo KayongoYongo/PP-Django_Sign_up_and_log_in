22b126161971bbf50d74987dce7346e79aa65220
KayongoYongo <sknsammie10@gmail.com>
2024-03-12 02:38:48 +0300

    added o-auth urls

--- sign_up_and_log_in/sign_up_and_log_in/urls.py
+++ sign_up_and_log_in/sign_up_and_log_in/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sign_up_and_log_in_app.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
